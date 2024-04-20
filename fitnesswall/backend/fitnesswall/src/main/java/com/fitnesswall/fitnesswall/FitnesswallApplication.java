8748d5066bc75d6b05ce8e335e6d434a279e104b
Tharaka Madushanka Wanshathilaka <it21169212@my.sliit.lk>
2024-04-20 17:29:03 +0530

    add backend

---
+++ fitnesswall/backend/fitnesswall/src/main/java/com/fitnesswall/fitnesswall/FitnesswallApplication.java
@@ -0,0 +1,13 @@
+package com.fitnesswall.fitnesswall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnesswallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnesswallApplication.class, args);
+	}
+
+}
